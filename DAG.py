90329c2bc29354219ec28054e9db70c0a310a983
rajpootmegha7 <68971156+rajpootmegha7@users.noreply.github.com>
2024-03-01 16:49:42 +0530

    GCS transfer of file

--- DAG.py
+++ DAG.py
@@ -3,6 +3,10 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
+import pandas as pd
+import os
+from google.cloud import storage
+
 
 # Define the default_args dictionary to specify the DAG's default parameters
 default_args = {
@@ -19,6 +23,13 @@ dag = DAG(
     description='DAG for COVID analysis with PySpark',
     schedule_interval='@daily',  # Adjust this based on your desired schedule
 )
+#Upload to gcs
+def upload_to_gcs(local_file_path, gcs_bucket_name, gcs_remote_dir):
+    """ Uploads a local file to a GCS bucket"""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(gcs_bucket_name)
+    blob = bucket.blob(gcs_remote_dir + file_path)
+    blob.upload_from_filename(local_file_path)
 
 # Define a PythonOperator to run the PySpark script
 def run_pyspark_script():
@@ -48,7 +59,49 @@ def run_pyspark_script():
     covid_df = spark.read.parquet(covid_data_path)
 
     # Continue with the rest of your script...
-    # ...
+    hospital_df = spark.read.parquet(hospital_data_path)
+    #hospital_df.show(10)
+    joined_df = covid_df.join(hospital_df,'City')
+
+    patient_count = covid_df.groupBy('City').agg(F.count('*').alias('p_count'))
+    #patient_count.show(10)
+
+    merged_df= joined_df.join(patient_count, 'City')
+    #merged_df.show(10)
+    poor_patient = joined_df.groupBy('City').agg(
+    (F.sum('COVID_Beds_Available') - F.count('*')).alias('Poor_Patients')
+    )
+    covid_bed_citywise = merged_df.groupBy('City').agg(F.sum('COVID_Beds_Available').alias('Covid_beds_citywise'))
+    new_df = covid_bed_citywise.join(merged_df, 'City', 'inner')
+    #new_df.show(10)
+    # Find the patient which didn't get the beds
+
+    pune_df = new_df.filter(new_df.City == 'Pune')
+    #pune_df.show(10)
+
+    unique_df = pune_df.dropDuplicates(['Hospital_Name'])
+
+    result_df = unique_df.withColumn( "patient_no_beds", F.col("Covid_beds_citywise")- F.col("p_count"))
+    #Write this result dataframe into the csv file for backup
+    result_df.write.csv('data/result_data/result.csv',header = True, mode= 'overwrite')
+    
+    #Store the resulted dataframe in the GCP bucket
+    file_path = '/Users/meghasinghrajpoot/Documents/GitHub/DataEngineeringProject/data/result_data/result.csv/result.csv'
+
+    df = pd.read_csv(file_path)
+    print(df)
+
+    credential_path = "msr-project-415908-4b94ba8da02e.json"
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+    
+    
+#Example usage
+    local_csv_file = file_path
+    gcs_bucket = 'msr_bucket'
+    gcs_directory = 'dag/'
+
+    upload_to_gcs(local_csv_file, gcs_bucket, gcs_directory)
+        
 
     # Don't forget to stop the Spark session at the end
     spark.stop()
--- ImportGCS.py
+++ ImportGCS.py
@@ -1,14 +1,28 @@
 import pandas as pd
+import os
 from google.cloud import storage #pip install google-cloud-storage
 
-df = pd.read_csv('data/result_data/result.csv/result.csv')
+file_path = '/Users/meghasinghrajpoot/Documents/GitHub/DataEngineeringProject/data/result_data/result.csv/result.csv'
 
+df = pd.read_csv(file_path)
 print(df)
 
-# Upload the data to cloud storage client 
-client = storage.Client()
-export_bucket = client.get_bucket('megha-gcp')
-#df.to_csv()
+credential_path = "msr-project-415908-4b94ba8da02e.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
-export_bucket.blob('result {0}.csv').upload_from_string(df.to_csv(),'text/csv')
+
+def upload_to_gcs(local_file_path, gcs_bucket_name, gcs_remote_dir):
+    """ Uploads a local file to a GCS bucket"""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(gcs_bucket_name)
+    blob = bucket.blob(gcs_remote_dir + file_path)
+    blob.upload_from_filename(local_file_path)
+    
+    
+#Example usage
+local_csv_file = file_path
+gcs_bucket = 'msr_bucket'
+gcs_directory = 'dag/'
+
+upload_to_gcs(local_csv_file, gcs_bucket, gcs_directory)
 
